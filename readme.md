515b194e073a06846e09f711ea5fb78d21a57394
Aydar Mullakhmatov <aydar-95@yandex.ru>
2022-04-10 22:09:22 +0500

    init

---
+++ readme.md
@@ -0,0 +1,3 @@
+#yaml-conf
+
+Модуль для чтения конфигурацию из YAML-файла в структуру с помощью библиотеки [go-yaml/yaml](https://github.com/go-yaml/yaml)
---
+++ yaml-conf.go
@@ -0,0 +1,72 @@
+package yamlconf
+
+import (
+	"fmt"
+	"log"
+	"net/url"
+	"os"
+
+	yaml "gopkg.in/yaml.v2"
+)
+
+type CustomURL url.URL
+
+func (cu *CustomURL) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	var urlString string
+
+	err := unmarshal(&urlString)
+	if err != nil {
+		return err
+	}
+
+	u, err := url.ParseRequestURI(urlString)
+	if err != nil {
+		return err
+	}
+
+	*cu = CustomURL(*u)
+	return nil
+}
+
+type AppConfig struct {
+	Port        int       `yaml:"port"`
+	DbURL       CustomURL `yaml:"db_url"`
+	JaegerURL   CustomURL `yaml:"jaeger_url"`
+	SentryURL   CustomURL `yaml:"sentry_url"`
+	KafkaBroker string    `yaml:"kafka_broker"`
+	SomeAppID   string    `yaml:"some_app_id"`
+	SomeAppKey  string    `yaml:"some_app_key"`
+}
+
+func (conf *AppConfig) Get(fileName string) {
+	if fileName == "" {
+		log.Fatalf("Не указано имя файла конфигурации")
+	}
+
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Fatalf("Ошибка при открытии файла конфигурации: %v", err)
+	}
+
+	defer func() {
+		err := file.Close()
+		if err != nil {
+			log.Fatalf("Ошибка при закрыттии файла конфигурации: %v\n", err)
+		}
+	}()
+
+	if err := yaml.NewDecoder(file).Decode(&conf); err != nil {
+		log.Fatalf("Ошибка при декодировании yaml-файла в структуру: %v\n", err)
+	}
+}
+
+func (conf *AppConfig) Print() {
+	fmt.Println("Configs:")
+	fmt.Println("Port: ", conf.Port)
+	fmt.Println("DBUrl: ", conf.DbURL)
+	fmt.Println("JaegerUrl: ", conf.JaegerURL)
+	fmt.Println("SentryUrl: ", conf.SentryURL)
+	fmt.Println("KafkaBroker: ", conf.KafkaBroker)
+	fmt.Println("SomeAppId: ", conf.SomeAppID)
+	fmt.Println("SomeAppKey: ", conf.SomeAppKey)
+}
